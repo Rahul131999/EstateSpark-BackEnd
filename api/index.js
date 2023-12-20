77edb626bdd7670d25bbb84835b535d9cbecaec7
rahul saharan <rahul.saharan@codeb.co.in>
2023-12-20 16:34:11 +0530

    mongoDB connected with the backend

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("DB sorted!!")
+}).catch((err)=>{
+    console.log(err)
+})
 
 const App = express();
 
-App.listen(3000,()=>{
+App.listen(process.env.PORT,()=>{
     console.log("server is up on 3000!!");
 })
